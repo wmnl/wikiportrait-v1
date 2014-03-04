f1eaa54cf3704443977bfea591544cb115f99eca
Hay Kranen <huskyr@gmail.com>
2014-03-04 14:55:31 +0100

    initial checkin

---
+++ _footer.php
@@ -0,0 +1,36 @@
+			<div id="footer">
+				<p>
+					The upload-wizard is programmed by <a href="http://nl.wikipedia.org/wiki/Gebruiker:Husky">Husky</a> and
+					released under the terms of the <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>.
+					Concept by the members <a href="http://nl.wikimedia.org/wiki/Werkgroep_vrije_media">Werkgroep Vrije Media</a>
+					(Workgroup Free Media) of the <a href="http://nl.wikimedia.org">Dutch chapter</a> of the Wikimedia Foundation.
+					The questions and explanations are written by <a href="http://nl.wikipedia.org/wiki/Gebruiker:Fruggo">Fruggo</a> 
+					and multi-licensed under both the <a href="http://www.gnu.org/copyleft/fdl.html">GNU Free Documentation License</a> and 
+					<a href="http://creativecommons.org/licenses/by-sa/2.5/">Creative Commons BY-SA 2.5</a>.
+				</p>
+			</div>
+		</div> <!-- /wrapper -->
+		
+		<?php
+            // only load this if we're not in a development environment :)
+            if(!is_development_mode()) :
+        ?>        
+        		<!-- Clicky webstats -->        
+            		<script src="http://static.getclicky.com/29850.js" type="text/javascript"></script>            		<noscript><p><img alt="Clicky" src="http://in.getclicky.com/29850-db4.gif" /></p></noscript>		
+        		<!-- /Clicky webstats -->
+        		<!-- Google Analytics -->
+                    <script type="text/javascript">
+                    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+                    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+                    </script>
+                    <script type="text/javascript">
+                    try {
+                    var pageTracker = _gat._getTracker("UA-8050986-7");
+                    pageTracker._trackPageview();
+                    } catch(err) {}</script>        		
+        		<!-- /Google Analytics -->
+		<?php
+            endif; 
+        ?>
+	</body>
+</html>
---
+++ inc/config-sample.php
@@ -0,0 +1,28 @@
+<?php
+	# config
+	define ('DB_HOST', 'localhost'); 		// host for your database. usually 'localhost' will do ok
+	define ('DB_USER', 'root'); 			// username for acces to the database
+	define ('DB_PASS', ''); 				// password for the database 
+	define ('DB_DATABASE', 'uploadwizard'); // the database you have made for the tables of this script	
+	
+	define ('TB_PREFIX', '');
+	define ('TB_MESSAGES', TB_PREFIX.'messages');
+	define ('TB_IMAGES', TB_PREFIX.'images');
+	
+	define ('GE_DEBUG', 0);	
+	define ('GE_LANGUAGE', 'nl'); 			// default language
+	
+	define ('ABSPATH', '/Users/hay/htdocs/uploadwizard/web/');
+	define ('GE_URL', 'http://localhost/uploadwizard/web/');
+	define ('GE_HOME', GE_URL.'index.php');
+	define ('GE_WIZARD', GE_URL.'wizard.php');
+	define ('GE_IMAGES_PATH', ABSPATH.'uploads/');
+	
+	$ALLOWED_EXTENSIONS = array(".png", ".jpg", ".jpeg", ".gif");	 // allowed extensions for image uploads, NOTE THE DOT (.) before every extension!
+	define ('GE_MIN_RESOLUTION', 480);
+	
+	define ('GE_OTRS_MAIL', 'info-nl@wikimedia.org');
+	define ('GE_OTRS_FROM', 'uploads@wikiportret.nl');
+	
+	error_reporting(0); // set this to 'E_ALL ^ E_NOTICE' for normal error reporting, SET TO ZERO FOR PRODUCTION USE!
+?>
